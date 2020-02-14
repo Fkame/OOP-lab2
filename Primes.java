ee92ca09abaf1e91f466710050e2e95f9a83b242
Fkame <nikitos_flame@mail.ru>
2020-02-14 23:29:10 +0300

    isPrime() + test fin

--- Primes.java
+++ Primes.java
@@ -1,7 +1,13 @@
 public class Primes {
 	
 	public static void main (String[] args) {
+		Primes primes = new Primes();
 		
+		boolean testing = primes.testIsPrime();
+		if (testing) 
+			System.out.println("Method is correct!");
+		else 
+			System.out.println("Method is not correct!");
 	}
 	
 	public static boolean isPrime(int n) {
@@ -21,15 +27,15 @@ public class Primes {
 	}
 	
 	private boolean testIsPrime() {
-		System.out.println("Testing isPrime method...")
+		System.out.println("Testing isPrime method...");
 		int[] testNum = {-6, -1, 0, 1, 2, 3, 4, 5, 22, 31};
 		boolean[] testAnswers = {true, false ,false, false, false, false, true, false, true, false};
 		
 		boolean isMethodCorrect = true;
 		
 		boolean answer;
-		for (int i = 0; i < testNum.lenght; i++) {
-			answer = isPrime(testNum[i]);
+		for (int i = 0; i < testNum.length; i++) {
+			answer = Primes.isPrime(testNum[i]);
 			if (answer != testAnswers[i]) isMethodCorrect = false;
 			
 			System.out.println("number: " + testNum[i] + ", correct answer: " + testAnswers[i] + ", method answer: " + answer);
