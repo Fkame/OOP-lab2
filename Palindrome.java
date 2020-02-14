42d320ee8d43c8b3e433d9d7588b4dd4aaa0ad5e
Fkame <nikitos_flame@mail.ru>
2020-02-15 00:35:12 +0300

    Palindrome-class finish

--- Palindrome.java
+++ Palindrome.java
@@ -5,11 +5,33 @@ public class Palindrome {
 		// Тестирование методов по переворачиванию строки
 		// Palindrome.testReverseString();
 		
+		System.out.println("Now this prog is cheching your arguments for the presence of palindromes...");
+		
+		if (args.length == 0) {
+			System.out.println("Wow, it seems like there isnt any args, so lets add some.");
+			args = Palindrome.addSomeStringValues();
+		}
+		
 		for (int i = 0; i < args.length; i++) {
 			String s = args[i];
+			
+			if (Palindrome.isPalindrome(s))
+				System.out.println((i + 1) + " | " + args[i] + " --> is palindrome");
+			else 
+				System.out.println((i + 1) + " | " + args[i] + " --> is not palindrome");
+		
 		}
 	}
 	
+	// Определяет, палиндром ли число (одинаковое написание при слева-направо и справа-налево)
+	public static boolean isPalindrome(String str) {
+		if (str.equals(reverseString(str))) 
+			return true;
+		else 
+			return false;
+	}
+	
+	// Функция посимвольного переворота строки
 	public static String reverseString(String str) {
 		int strLen = str.length();
 		
@@ -22,7 +44,7 @@ public class Palindrome {
 		return new String(reStr);
 	}
 	
-	
+	// Функция вывода результата работы функций переворота строк
 	private static void testReverseString() {
 		String one = "abcdef";
 		System.out.println(one + " --> " + reverseString(one) + " || " + reverseStringSimple(one));
@@ -32,10 +54,27 @@ public class Palindrome {
 		System.out.println(three + " --> " + reverseString(three) + " || " + reverseStringSimple(three));
 	}
 	
-	
+	// Переворот строки с помощью функциронала StringBuffer
 	public static String reverseStringSimple(String str) {
 		StringBuffer rstr = new StringBuffer(str);
 		return new String(rstr.reverse());
+	}
+	
+	// На случай, если нет входных аргументов - и для теста ещё.
+	public static String[] addSomeStringValues() {
+		String[] args = new String[10];
+		args[0] = "Hi";
+		args[1] = "WoW";
+		args[2] = "My name is";
+		args[3] = "What?";
+		args[4] = "I said it";
+		args[5] = "heh";
+		args[6] = "Bob";
+		args[7] = "i means";
+		args[8] = "bob";
+		args[9] = "that is it";
+		
+		return args;
 	}
 	
 }
